44c3d6fbb2f1ff102897da66c27ff10506653414
e.j. (they/them) <git@ejs.sh>
2023-10-15 16:23:39 -0700

    goci v1, initial build

---
+++ gocli/processes/goci/go.mod
@@ -0,0 +1,3 @@
+module github.com/ejsdotsh/learning/gocli/processes/goci
+
+go 1.21.1
---
+++ gocli/processes/goci/main_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+	var testCases = []struct {
+		name   string
+		proj   string
+		out    string
+		expErr error
+	}{
+		{name: "success", proj: "./testdata/tool/",
+			out:    "go build: success\n",
+			expErr: nil},
+		{name: "fail", proj: "./testdata/toolErr",
+			out:    "",
+			expErr: &stepErr{step: "go build"}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var out bytes.Buffer
+			err := run(tc.proj, &out)
+
+			if tc.expErr != nil {
+				if err == nil {
+					t.Errorf("expected error: %q. got 'nil' instead.", tc.expErr)
+					return
+				}
+
+				if !errors.Is(err, tc.expErr) {
+					t.Errorf("expected error: %q. got %q.", tc.expErr, err)
+				}
+				return
+			}
+
+			if err != nil {
+				t.Errorf("unexpected error: %q", err)
+			}
+
+			if out.String() != tc.out {
+				t.Errorf("expected output: %q. got %q", tc.out, out.String())
+			}
+		})
+	}
+}
